0a6c70266e54c72ea7603bcae452213de8e6bf76
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-16 21:34:49 -0500

    Feat: configuration maps

--- ios/mapsapp/AppDelegate.mm
+++ ios/mapsapp/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDhc_gpGO9ePeXplKsZUZR4Y8XBAgHY4Sw"];
   self.moduleName = @"mapsapp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
