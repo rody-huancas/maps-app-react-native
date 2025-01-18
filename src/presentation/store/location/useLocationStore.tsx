0ee97ce3834feb862974d6c829eeca66715266ba
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-17 23:33:43 -0500

    Feat: watch location

--- src/presentation/store/location/useLocationStore.tsx
+++ src/presentation/store/location/useLocationStore.tsx
@@ -1,18 +1,50 @@
 import {create} from 'zustand';
 import {Location} from '../../../infrastructure/interfaces/location';
-import {getCurrentLocation} from '../../../actions/location/location';
+import {
+  clearWatchLocation,
+  getCurrentLocation,
+  watchCurrentLocation,
+} from '../../../actions/location/location';
 
 interface LocationState {
   lastKnowLocation: Location | null;
+  userLocationList: Location[];
+  watchId: number | null;
 
   getLocation: () => Promise<Location | null>;
+  watchLocation: () => void;
+  clearWatchLocation: () => void;
 }
 
 export const useLocationStore = create<LocationState>()((set, get) => ({
   lastKnowLocation: null,
+  userLocationList: [],
+  watchId: null,
+
   getLocation: async () => {
     const location = await getCurrentLocation();
     set({lastKnowLocation: location});
     return location;
+  },
+  watchLocation: () => {
+    const watchId = get().watchId;
+    if (watchId !== null) {
+      get().clearWatchLocation();
+    }
+
+    const id = watchCurrentLocation(location => {
+      set({
+        lastKnowLocation: location,
+        userLocationList: [...get().userLocationList, location],
+      });
+    });
+
+    set({watchId: id});
+  },
+  clearWatchLocation: () => {
+    const watchId = get().watchId;
+    if (watchId !== null) {
+      clearWatchLocation(watchId);
+    }
   },
 }));
